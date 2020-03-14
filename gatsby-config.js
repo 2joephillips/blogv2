4cb5ef274b7334433d72bf2bdc4f3fc833a5ddce
joseph e phillips <joseph@josephephillips.com>
2020-03-14 14:42:56 -0400

    wire up contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,23 @@
+require("dotenv").config()
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: process.env.CONTENTFUL_HOST,
+}
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  )
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Joseph E Phillips`,
+    description: `Father | Husband | Programmer`,
+    author: `@2joephillips`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +41,10 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: contentfulConfig,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
