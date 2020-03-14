10d4ef666ad6d64ed5fc31809cf4f3e839440d2e
joseph e phillips <joseph@josephephillips.com>
2020-03-14 19:23:34 -0400

    update to use Bulma header layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,50 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import '../styles/main.scss';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="https://bulma.io">
+        {siteTitle}
+      </a>
+
+      <label
+        role="button"
+        className="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navBar"
+        htmlFor="nav-toggle-state"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </label>
     </div>
-  </header>
-)
+    <input type="checkbox" id="nav-toggle-state" />
+
+    <div id="navbar" className="navbar-menu">
+      <div className="navbar-start">
+        <Link to="/" className="navbar-item">
+          Home
+        </Link>
+
+        <Link to="/blog" className="navbar-item">
+          Blog
+        </Link>
+      </div>
+    </div>
+  </nav>
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import '../styles/main.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,31 +14,37 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <section className="hero is-fullheight">
+        <div className="hero-head">
+          <Header siteTitle={data.site.siteMetadata.title} />
+        </div>
+
+        <div className="hero-body">
+          <div className="container">
+            <main>{children}</main>
+          </div>
+        </div>
+
+        <div className="hero-foot">
+          <footer className="footer">
+            <div className="content has-text-centered">
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </div>
+          </footer>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
