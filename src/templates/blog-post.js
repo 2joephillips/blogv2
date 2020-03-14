c6b2dea6e056cc336ca25ccc636e2c52bad8f365
joseph e phillips <joseph@josephephillips.com>
2020-03-14 15:18:12 -0400

    create post pages

---
+++ src/templates/blog-post.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
+import get from 'lodash/get';
+import Img from 'gatsby-image';
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = get(this.props, 'data.contentfulBlogPost');
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+
+    return (
+      <div style={{ background: '#fff' }}>
+        <Helmet title={`${post.title} | ${siteTitle}`} />
+        <Img alt={post.title} fluid={post.heroImage.fluid} />
+        <div className="wrapper">
+          <h1 className="section-headline">{post.title}</h1>
+          <p
+            style={{
+              display: 'block',
+            }}
+          >
+            By: {post.author.name} on {post.publishDate}
+          </p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.body.childMarkdownRemark.html,
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      author {
+        name
+      }
+      publishDate(formatString: "MMMM Do, YYYY")
+      heroImage {
+        fluid(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
